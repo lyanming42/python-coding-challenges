812278085bfd0abbc5897f2ba3b62b414c4315c6
YanMing Li <lyanming42@gmail.com>
2025-02-20 17:49:57 -0800

    add more examples

--- regex.py
+++ regex.py
@@ -26,9 +26,45 @@ Matches any non-alphanumeric character; this is equivalent to the class [^a-zA-Z
 
 """
 
+"""
+
+"""
+
 def main():
     print("main")
     print(re.ASCII)
+    print(re.DOTALL)
+
+    re.compile("ab*")
+
+    p = re.compile('[a-z]+')
+    print(p)
+
+    m = p.match('tempo')
+    print(m.group())
+    print(m.start())
+    print(m.end())
+
+    print(m.span())
+
+    print(re.findall(r"\d+", "12312 1231231 abcdfwfwe aa 12312"))
+    print(re.finditer(r"\d+", "12312 1231231 abcdfwfwe aa 12312"))
+
+    p = re.compile('(a(b)c)d')
+    m = p.match('abcd')
+    print(m.group(2))
+
+    p = re.compile(r'\b(\w+)\s+\1\b')
+    print(p.search('Paris in the the spring').group())
+
+    m = re.match("([abc])+", "abc")
+    print(m.groups())
+
+    m = re.match("(?:[abc])+", "abc")
+    print(m.groups())
+
+    p = re.compile(r'(?P<word>\b\w+\b)')
+
 
 if __name__ == "__main__":
     main()
